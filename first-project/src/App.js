b90e2325f5c65567f4723804ed61b02438bf1285
caua-3301 <134548536+caua-3301@users.noreply.github.com>
2024-03-01 22:33:29 -0300

    first commit

---
+++ first-project/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //JSX, tudo isso é convertido com Babel par JS, que o browser entende
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
